7141e60ffa99114f1457d84c4e5417de12e7b4d6
OverCloud-Systems <157417255+OverCloud-Systems@users.noreply.github.com>
2024-01-30 22:22:18 +0000

    Update | Search engines! + large improvements

--- public/js/home.js
+++ public/js/home.js
@@ -38,6 +38,11 @@ function uv() {
 
 const form = document.querySelector('form');
 const input = document.querySelector('input');
+const engine = localStorage.getItem('engine');
+if (engine === null) {
+    localStorage.setItem('engine', 'Brave');
+    engine = localStorage.getItem('engine');
+}
 
 form.addEventListener('submit', async event => {
     event.preventDefault();
@@ -47,7 +52,17 @@ form.addEventListener('submit', async event => {
         let url = input.value.trim();
             
         if (!isUrl(url))
-            url = 'https://search.brave.com/search?q=' + url;
+            if (engine === "Brave") {
+                url = 'https://search.brave.com/search?q=' + url;
+            } else if (engine === "Google") {
+                url = 'https://google.com/search?q=' + url;
+            } else if (engine === "DuckDuckGo") {
+                url = 'https://duckduckgo.com/search?q=' + url;
+            } else if (engine === "Bing") {
+                url = 'https://bing.com/search?q=' + url;
+            } else if (engine === "Boogle") {
+                url = 'https://sites.google.com/search/butter-dog-search?query=' + url;
+            }
         else if (!(url.startsWith('https://') || url.startsWith('http://')))
             url = 'http://' + url;
         
@@ -95,93 +110,3 @@ if (localStorage.hasOwnProperty("title")) {
 if (localStorage.hasOwnProperty("favicon")) {
     document.querySelector("link[rel='shortcut icon']").href = favicon;
 }
-
-/* Cloud Stuff */
-
-// function setGameCloud(){
-//     let left = document.getElementById('games_cloud').style.left;
-//     /* console.log(left); */
-//     localStorage.setItem('games_cloud', left);
-
-
-//     let games_cloud = localStorage.getItem("games_cloud");
-//     console.log('Games Cloud LocalStorage : ' + games_cloud);
-// }
-
-// function setSettingsCloud(){
-
-//     let left = document.getElementById('settings_cloud').style.left;
-//     /* console.log(left); */
-//     localStorage.setItem('settings_cloud', left);
-
-
-//     let settings_cloud = localStorage.getItem("settings_cloud");
-//     console.log('Settings Cloud LocalStorage : ' + settings_cloud);
-
-// }
-
-
-// let games_cloud = localStorage.getItem("games_cloud");
-// let settings_cloud = localStorage.getItem("settings_cloud");
-
-
-// if (localStorage.hasOwnProperty("games_cloud")) {
-//     document.getElementById('games_cloud').style.left = games_cloud;
-// }
-
-// if (localStorage.hasOwnProperty("settings_cloud")) {
-//     document.getElementById('settings_cloud').style.left = settings_cloud;
-// }
-
-
-
-// if (localStorage.getItem("home_cloud_speed") === null) {
-//     localStorage.setItem("home_cloud_speed", 0.3)
-// }
-
-// if (localStorage.getItem("games_cloud_speed") === null) {
-//     localStorage.setItem("games_cloud_speed", 0.5)
-// }
-
-
-// if (localStorage.getItem("settings_cloud_speed") === null) {
-//     localStorage.setItem("settings_cloud_speed", 0.3)
-// }
-
-
-/* Cloud Movement stuff */
-
-// setInterval(function(){
-
-//     /* console.log((Number(cloud.style.left.slice(0, -2)) + ).toString() + 'px'); */
-
-//     var x = document.getElementsByClassName("clouds");
-//     for (var i = 0; i < x.length; i++) {
-
-//         cloud = x[i];
-
-
-//         if(cloud.id == 'home_cloud') {
-//             let home_cloud_speed = localStorage.getItem("home_cloud_speed")
-
-//             cloud.style.left = (Number(cloud.style.left.slice(0, -2)) + Number(home_cloud_speed)).toString() + 'px';
-//         } else if(cloud.id == 'games_cloud') {
-//             let games_cloud_speed = localStorage.getItem("games_cloud_speed")
-
-//             cloud.style.left = (Number(cloud.style.left.slice(0, -2)) + Number(games_cloud_speed)).toString() + 'px';
-
-//         } else{
-
-//             let settings_cloud_speed = localStorage.getItem("settings_cloud_speed")
-
-//             cloud.style.left = (Number(cloud.style.left.slice(0, -2)) + Number(settings_cloud_speed)).toString() + 'px';
-
-
-//         }
-//         if ($(window).width() < Number(cloud.style.left.slice(0, -2))) {
-
-//             cloud.style.left = '-200px';
-
-//         }
-//     }
-// }, 10);
--- public/js/settings.js
+++ public/js/settings.js
@@ -23,6 +23,8 @@ console.log(' ______ __ __ __                 \n' +
 var title = localStorage.getItem("title");
 var favicon = localStorage.getItem("favicon");
 var theme = localStorage.getItem("theme") || "OverCloud";
+var prox = localStorage.getItem("corrosion");
+var engine = localStorage.getItem("engine");
 
 if (localStorage.hasOwnProperty("title")) {
   document.title = title;
@@ -36,13 +38,29 @@ if (!localStorage.hasOwnProperty("theme")) {
   localStorage.setItem("theme","OverCloud");
 }
 
+if (!localStorage.hasOwnProperty("engine")) {
+  localStorage.setItem("engine","Brave");
+} else {
+  document.getElementById("engine_select").value = engine;
+}
+
 function settitle(title) {
   if (title !== "") {
-    localStorage.setItem("title", title)
-    document.title = title
+    localStorage.setItem("title", title);
+    document.title = title;
+    try {
+      window.top.postMessage(`title-${title}`, '*')
+    } catch (err) {
+      console.warn("No iframe found.");
+    }
   } else {
-    localStorage.removeItem("title")
-    document.title = "\u200E"
+    localStorage.removeItem("title");
+    document.title = "\u200E";
+    try {
+      window.top.postMessage('title-Calendar', '*')
+    } catch (err) {
+      console.warn("No iframe found.");
+    }
   }
 }
 
@@ -62,7 +80,7 @@ function setgoogle() {
 }
 
 function setgoogled() {
-  settitle("Google Drive");
+  settitle("Home - Google Drive");
   setfavicon("https://www.drive.google.com/favicon.ico");
 }
 
@@ -82,24 +100,40 @@ function setreset() {
   location.reload();
 }
 
-function setTheme(name) {
-  // const themes = ["OverCloud", "Google"];
-  // if (themes.includes(name)) {
-    localStorage.setItem("theme", name);
-  // }
-}
-
 const themes_set = document.getElementById("themes_select");
 themes_set.value = theme;
-themes_set.onchange = (event) =>{
-  setTheme(themes_set.value);
+
+const prox_set = document.getElementById("prox_select");
+if (prox === true) {
+  prox_set.value = "corrosion";
+} else {
+  prox_set.value = "womginx";
+}
+
+function setTheme(name) {
+  localStorage.setItem("theme", name);
+}
+
+function setProx(name) {
+  if (name === "corrosion"){
+    localStorage.setItem("corrosion", true);
+    prox_set.value = "corrosion";
+  } else {
+    if (name === "womgnix") {
+      localStorage.setItem("corrosion", false);
+      prox_set.value = "womginx";
+    }
+  }
+}
+
+function setEngine(name) {
+  localStorage.setItem("engine", name);
 }
 
 // function setGameCloud(){
 //   let left = document.getElementById('games_cloud').style.left;
 //   /* console.log(left); */
 //   localStorage.setItem('games_cloud', left);
-
 
 //   let games_cloud = localStorage.getItem("games_cloud");
 //   console.log('Games Cloud LocalStorage : ' + games_cloud);
@@ -111,16 +145,13 @@ themes_set.onchange = (event) =>{
 //   /* console.log(left); */
 //   localStorage.setItem('home_cloud', left);
 
-
 //   let home_cloud = localStorage.getItem("home_cloud");
 //   console.log('Homes Cloud LocalStorage : ' + home_cloud);
 
 // }
 
-
 // let games_cloud = localStorage.getItem("games_cloud");
 // let home_cloud = localStorage.getItem("home_cloud");
-
 
 // if (localStorage.hasOwnProperty("games_cloud")) {
 //     document.getElementById('games_cloud').style.left = games_cloud;
@@ -130,8 +161,6 @@ themes_set.onchange = (event) =>{
 //     document.getElementById('home_cloud').style.left = home_cloud;
 // }
 
-
-
 // var homeSlider = document.getElementById("Home");
 
 // homeSlider.value = Number(localStorage.getItem("home_cloud_speed") * 100);
@@ -140,7 +169,6 @@ themes_set.onchange = (event) =>{
 //   console.log(this.value);
 //   localStorage.setItem("home_cloud_speed", (this.value / 100));
 // } 
-
 
 // var gamesSlider = document.getElementById("Games");
 
@@ -160,7 +188,6 @@ themes_set.onchange = (event) =>{
 //   localStorage.setItem("settings_cloud_speed", (this.value / 100));
 // } 
 
-
 // if (localStorage.getItem("home_cloud_speed") === null) {
 //   localStorage.setItem("home_cloud_speed", 0.3)
 // }
@@ -169,95 +196,45 @@ themes_set.onchange = (event) =>{
 //   localStorage.setItem("games_cloud_speed", 0.5)
 // }
 
-
 // if (localStorage.getItem("settings_cloud_speed") === null) {
 //   localStorage.setItem("settings_cloud_speed", 0.3)
 // }
 
 let corrosion_check = true
-let corrosion_or_womginx = document.getElementById('corrosion_or_womginx')
-var corrosion = document.getElementById("corrosion");
+// var corrosion = document.getElementById("corrosion");
 
 if(localStorage.getItem('corrosion') === 'true'){
-  console.log('Corrosion')
-
-  corrosion_or_womginx.textContent = 'Corrosion'
   corrosion_check = true
-
-
 } else {
-  console.log('Womginx')
-
-  corrosion_or_womginx.textContent = 'Womginx'
   corrosion_check = false
-
 }
 
-
-corrosion.oninput = function() {
-  console.log(this.checked);
-  if(corrosion_check === true){
-    console.log('Womginx')
-
-    corrosion_or_womginx.textContent = 'Womginx'
-
-    localStorage.setItem("corrosion", false);
-    corrosion_check = false
-
-
-  } else {
-
-
-    console.log('Corrosion')
-
-    corrosion_or_womginx.textContent = 'Corrosion'
-
-    localStorage.setItem("corrosion", true);
-    corrosion_check = true
-
-  }
-
-  this.checked = false;
-
-}
-
+// corrosion.oninput = function() {
+//   if(corrosion_check === true){
+//     localStorage.setItem("corrosion", false);
+//   } else {
+//     localStorage.setItem("corrosion", true);
+//     corrosion_check = true
+//   }
+// }
 
 setInterval(function(){
-
   /* console.log((Number(cloud.style.left.slice(0, -2)) + ).toString() + 'px'); */
-
   var x = document.getElementsByClassName("clouds");
   for (var i = 0; i < x.length; i++) {
-
     cloud = x[i];
-
-
     if(cloud.id == 'home_cloud') {
       let home_cloud_speed = localStorage.getItem("home_cloud_speed")
-      
       cloud.style.left = (Number(cloud.style.left.slice(0, -2)) + Number(home_cloud_speed)).toString() + 'px';
     } else if(cloud.id == 'games_cloud') {
       let games_cloud_speed = localStorage.getItem("games_cloud_speed")
-      
       cloud.style.left = (Number(cloud.style.left.slice(0, -2)) + Number(games_cloud_speed)).toString() + 'px';
-
     } else{
-
       let settings_cloud_speed = localStorage.getItem("settings_cloud_speed")
-      
       cloud.style.left = (Number(cloud.style.left.slice(0, -2)) + Number(settings_cloud_speed)).toString() + 'px';
-
-
     }
     if ($(window).width() < Number(cloud.style.left.slice(0, -2))) {
-
       cloud.style.left = '-200px';
-
     }
   }
 }, 10);
-
-
-
-
-
