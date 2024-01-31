aafa6c7e7359f6f36fa2608258ebf3b02d38d81d
OverCloud-Systems <157417255+OverCloud-Systems@users.noreply.github.com>
2024-01-31 19:25:07 +0000

    Update | Renamed files.

--- public/service/sw.js
+++ public/service/sw.js
@@ -1,5 +1,5 @@
-importScripts('/uv/uv.bundle.js');
-importScripts('/uv/uv.config.js');
+importScripts('/service/bundle.js');
+importScripts('/service/config.js');
 
 class UVServiceWorker extends EventEmitter {     
     constructor(config = __uv$config) {
